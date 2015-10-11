61999ed025b5a2b9d4b40a9dbd8355435ebb570f
yuzx2008 <yuzx2008@gmail.com>
2015-10-11 17:15:43 +0800

    add .

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,20 +31,16 @@ function doIt() {
   fi
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-  echo "dotfiles installed completed. Please don't forget to change your git username and email:";
-  echo "    git config --global user.name \"Your Name\"";
-  echo "    git config --global user.email you@example.com";
-  echo "";
-  echo "When you first start vim, please use :PluginInstall to install all the plugins.(vim +PluginInstall +qall)"
-  echo "Have fun!"
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  doIt;
 fi;
+echo "dotfiles installed completed. Please don't forget to change your git username and email:";
+echo "    git config --global user.name \"Your Name\"";
+echo "    git config --global user.email you@example.com";
+echo "";
+echo "When you first start vim, please use :PluginInstall to install all the plugins.(vim +PluginInstall +qall)"
+echo "Have fun!"
 
 unset doIt;
