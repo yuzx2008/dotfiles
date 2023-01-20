fcc7377c15e179bd277c37a37e03bb4dc267b86f
yuzx2008 <yuzx2008@gmail.com>
2023-01-20 09:50:39 +0800

    add .

---
+++ .config/systemd/user/run_gopls.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -x
+
+export GOROOT=/usr/local/go
+export GOPROXY=https://goproxy.cn
+export GOPRIVATE=gitlab.bj.sensetime.com/diamond/*
+export GOPATH=/home/yuzx/go
+export GO111MODULE=on
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+export SHELL=/usr/local/bin/zsh
+export HOME=/home/yuzx
+
+gopls serve -listen=127.0.0.1:60098 -logfile=/tmp/gopls.log -debug=:60099
